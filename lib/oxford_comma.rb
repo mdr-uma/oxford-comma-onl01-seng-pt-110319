6e2ed00876986f48882fbda62d37b70011d2e459
Uma Manandhar <um_2003_8@hotmail.com>
2020-10-12 03:35:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(' and ')
   else
-    return array(0..-2).join(', ') + ", and " + array[-1]
+    return array[0..-2].join(', ') + ", and " + array[-1]
   end
 end
